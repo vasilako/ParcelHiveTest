f457057b99a1b9339355b89b00fdacd26aca1fb3
vasilako <vasilako@hotmail.com>
2024-06-01 15:58:59 +0300

    Create Flask app, configure, secret_key, create socket to listen to the app, configure serial port, establish connection to database, create 2 tables running 2 querys

--- server.py
+++ server.py
@@ -6,4 +6,21 @@ import base64
 import sqlite3
 import time
 import serial
+import os
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+socketio = SocketIO(app)
+
+# Ahora configuraremos la conexion serial
+ser = serial.Serial('COM3', 9600)
+
+# Después deberiamos configurar la conexion de la base de datos
+conn = sqlite3.connect("my_sqlite.sqlite", check_same_thread=False)
+
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS mouse_data
+             (x INTEGER, y INTEGER, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+c.execute('''CREATE TABLE IF NOT EXISTS images
+             (image BLOB, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
+c.commit()
