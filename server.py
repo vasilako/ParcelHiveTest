a44273dd9741e01e8197aebf119fb4b3ffe94471
vasilako <vasilako@hotmail.com>
2024-06-01 20:42:53 +0300

    Refactoring of the whole code, now it works fine.

--- server.py
+++ server.py
@@ -8,42 +8,47 @@ import time
 import serial
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = os.urandom(24)
 socketio = SocketIO(app)
 
-# Ahora configuraremos la conexion serial
-ser = serial.Serial('COM3', 9600)
+# Configurar la conexión serial
+serial_port = '/dev/tty.Bluetooth-Incoming-Port'
+baud_rate = 9600
+try:
+    ser = serial.Serial(serial_port, baud_rate)
+    print(f"Serial port {serial_port} opened successfully.")
+except serial.SerialException as e:
+    print(f"Error opening serial port {serial_port}: {e}")
+    ser = None
 
-# Después deberiamos configurar la conexion de la base de datos
+# Configurar la conexión de la base de datos
 conn = sqlite3.connect("my_sqlite.sqlite", check_same_thread=False)
-
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS mouse_data
              (x INTEGER, y INTEGER, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 c.execute('''CREATE TABLE IF NOT EXISTS images
              (image BLOB, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
-c.commit()
+conn.commit()
 
-
-# Esta funcción leerá los datos del pueto serial
+# Función para leer datos del puerto serial
 def serial_reader():
     while True:
-        if ser.in_waiting > 0:
+        if ser and ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             x, y = map(int, line.split(','))
             socketio.emit('mouse_position', {'x': x, 'y': y})
-            c.execute("INSERT INTO mouse_data (x, y) VALUES (?, ?)", (x, y))
-            conn.commit()
+            # c.execute("INSERT INTO mouse_data (x, y) VALUES (?, ?)", (x, y))
+            # conn.commit()
 
 threading.Thread(target=serial_reader).start()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@socketio.on('take_ficture')
-def take_ficture(data):
+@socketio.on('take_picture')
+def take_picture(data):  # Cambié el nombre de la función a 'take_picture' para que coincida con el evento
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     cap.release()
@@ -55,7 +60,14 @@ def take_ficture(data):
         conn.commit()
         emit('picture', {'image': image_base64})
 
-
+@socketio.on('click')
+def handle_mouse_click(data):
+    x = data['x']
+    y = data['y']
+    print("Coordenadas del clic del ratón:", x, y)
+    socketio.emit('mouse_position', {'x': x, 'y': y})
+    c.execute("INSERT INTO mouse_data (x, y) VALUES (?, ?)", (x, y))
+    conn.commit()
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
