3616d2fdc0af993c828ec3bad79075317618ebc8
vasilako <vasilako@hotmail.com>
2024-06-03 12:32:56 +0300

    First commit, after refactoring the code with the following changes. The images are already saved to the server, and only the url is sent to the browser. Added css file to fit the image to the screen. In the index.html a link for css has been added. Base image has been included. Modified .gitignore so that it does not upload images to GitHub.

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 from flask_socketio import SocketIO, emit
 import threading
 import cv2
-import base64
 import sqlite3
-import time
 import serial
+import datetime as dt
+import time
+import logging
 import os
 
+import base64
+
+
 app = Flask(__name__, template_folder='templates')
-app.config['SECRET_KEY'] = os.urandom(24)
-socketio = SocketIO(app)
+app.config['SECRET_KEY'] =  os.urandom(24) #'secret!'
+logging.basicConfig(level=logging.DEBUG)
+socketio = SocketIO(app, cors_allowed_origins="*", logger=True, engineio_logger=True)
 
 # Configurar la conexión serial
+
+# todo: Hay que comprobar todos los puertos usb, bluetooth, y serial com3
 serial_port = '/dev/tty.Bluetooth-Incoming-Port'
 baud_rate = 9600
 try:
@@ -31,6 +38,8 @@ c.execute('''CREATE TABLE IF NOT EXISTS images
              (image BLOB, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 conn.commit()
 
+
+
 # Función para leer datos del puerto serial
 def serial_reader():
     while True:
@@ -38,8 +47,7 @@ def serial_reader():
             line = ser.readline().decode('utf-8').rstrip()
             x, y = map(int, line.split(','))
             socketio.emit('mouse_position', {'x': x, 'y': y})
-            # c.execute("INSERT INTO mouse_data (x, y) VALUES (?, ?)", (x, y))
-            # conn.commit()
+
 
 threading.Thread(target=serial_reader).start()
 
@@ -47,18 +55,58 @@ threading.Thread(target=serial_reader).start()
 def index():
     return render_template("index.html")
 
+
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    return send_from_directory('images', filename)
+
+
+# @socketio.on('take_picture')
+# def take_picture(data):  # Cambié el nombre de la función a 'take_picture' para que coincida con el evento
+#     cap = cv2.VideoCapture(0)
+#     ret, frame = cap.read()
+#     cap.release()
+#
+#     if ret:
+#         _, buffer = cv2.imencode('.jpg', frame)
+#         image_base64 = base64.b64encode(buffer).decode('utf-8')
+#         c.execute("INSERT INTO images (image) VALUES (?)", (buffer.tobytes(),))
+#         conn.commit()
+#         emit('picture', {'image': image_base64})
+#         print(frame.dtype)
+#
+#     else:
+#         print("Failed to capture image")
+
+
+
 @socketio.on('take_picture')
-def take_picture(data):  # Cambié el nombre de la función a 'take_picture' para que coincida con el evento
+def take_picture(data):
     cap = cv2.VideoCapture(0)
+    time.sleep(2)
     ret, frame = cap.read()
     cap.release()
 
     if ret:
         _, buffer = cv2.imencode('.jpg', frame)
-        image_base64 = base64.b64encode(buffer).decode('utf-8')
         c.execute("INSERT INTO images (image) VALUES (?)", (buffer.tobytes(),))
         conn.commit()
-        emit('picture', {'image': image_base64})
+
+        # Formatear image path
+        now = dt.datetime.now()
+        date_time_format = "%Y-%m-%d-%H-%M-%S"
+        image_path = f"images/imagen{now.strftime(date_time_format)}.jpg"
+
+        # Guardar la imagen en el servidor
+        cv2.imwrite(image_path, frame)
+
+        # Generar la URL de la imagen
+        image_url = image_path  # Reemplaza con la URL real de tu servidor
+
+        # Enviar la URL de la imagen al cliente
+        emit('picture', {'url': image_url})
+        print(image_url)
+
 
 @socketio.on('click')
 def handle_mouse_click(data):
@@ -68,6 +116,7 @@ def handle_mouse_click(data):
     socketio.emit('mouse_position', {'x': x, 'y': y})
     c.execute("INSERT INTO mouse_data (x, y) VALUES (?, ?)", (x, y))
     conn.commit()
+
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
