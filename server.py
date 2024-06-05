1dd638ccdba26a4dbf1993e7f4e4b1b3008c4f24
vasilako <vasilako@hotmail.com>
2024-06-05 08:34:13 +0300

    Remove port from Procfile and server.py

--- server.py
+++ server.py
@@ -88,4 +88,4 @@ def handle_mouse_click(data):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', debug=True, allow_unsafe_werkzeug=True)
